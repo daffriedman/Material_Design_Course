737f591caa4b0a08d5c7a76f9311008de794c30b
Daniel Friedman <dafriedman443@gmail.com>
2020-02-03 13:00:22 +0200

    added a seperate module for the material design which is what you should do

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {MatButtonModule} from '@angular/material';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import{MaterialModule} from './material/material.module';
+import { MatButtonModule } from '@angular/material';
 @NgModule({
   declarations: [
     AppComponent
@@ -13,7 +14,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatButtonModule
+    MaterialModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
